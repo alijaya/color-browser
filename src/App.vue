63f55ca6604d58b4a782189312b0c31e131909b5
Ali Jaya Meilio <alijayameilio@gmail.com>
2021-10-09 21:32:03 +0700

    integration succeeded

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,103 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div v-if="isSignedIn">
+      <button @click="logout()" type="button">Logout</button>
+      {{ userName }}
+    </div>
+    <button
+      :disabled="isSignedIn === null"
+      @click="login()"
+      type="button"
+      v-if="!isSignedIn"
+    >
+      Login
+    </button>
+    <template v-if="isSignedIn">
+      <button @click="fetchFiles()">Fetch Files</button>
+      <ul>
+        <li v-for="file in files" :key="file.id">
+          <a :href="file.webViewLink" target="_blank">
+            <img :src="file.thumbnailLink || file.iconLink" />
+            <span>{{ file.name }}</span>
+          </a>
+          <img
+            v-if="file.mimeType == 'image/jpeg'"
+            :src="file.webContentLink"
+            style="display:block; max-width:100%"
+          />
+          <button
+            v-if="file.mimeType == 'application/vnd.google-apps.folder'"
+            @click="fetchChildFiles(file.id)"
+          >
+            Fetch Files
+          </button>
+          <ul>
+            <li v-for="childFile in childFiles[file.id]" :key="childFile.name">
+              {{ childFile.name }}
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </template>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      isSignedIn: null,
+      files: [],
+      childFiles: {},
+    };
+  },
+  created() {
+    this.$gapi.listenUserSignIn((isSignedIn) => {
+      this.isSignedIn = isSignedIn;
+    });
+  },
+  methods: {
+    login() {
+      this.$gapi.login();
+    },
+    logout() {
+      this.$gapi.logout();
+    },
+    async fetchFiles() {
+      const gapi = await this.$gapi.getGapiClient();
+      const response = await gapi.client.drive.files.list({
+        q: "mimeType = 'application/vnd.google-apps.folder'",
+        pageSize: 10,
+        fields:
+          "nextPageToken, files(id, name, iconLink, thumbnailLink, webViewLink, webContentLink, mimeType, parents)",
+      });
+      this.files = response.result.files;
+    },
+
+    async fetchChildFiles(folderId) {
+      const gapi = await this.$gapi.getGapiClient();
+      const response = await gapi.client.drive.files.list({
+        q: `'${folderId}' in parents`,
+        pageSize: 10,
+        fields:
+          "nextPageToken, files(id, name, iconLink, thumbnailLink, webViewLink, webContentLink, mimeType, parents)",
+      });
+      this.childFiles[folderId] = response.result.files;
+    },
+  },
+  computed: {
+    userName() {
+      const user = this.$gapi.getUserData();
+
+      if (user) {
+        return user.fullName;
+      }
+      return null;
+    },
+  },
+};
 </script>
 
 <style>
@@ -19,8 +105,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import VueGapi from "vue-gapi";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(VueGapi, {
+  apiKey: process.env.VUE_APP_G_API_KEY,
+  clientId: process.env.VUE_APP_G_CLIENT_ID,
+  discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
+  scope: "https://www.googleapis.com/auth/drive.metadata.readonly",
+});
+app.mount("#app");
